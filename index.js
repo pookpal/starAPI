f5b885c186290991b2970832af3ce70b76accf8a
pookpal <pookpal@163.com>
2016-06-15 11:58:46 +0800

    first code

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lijie on 16/6/15.
+ */
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.use('/api', express.static('api'));
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
